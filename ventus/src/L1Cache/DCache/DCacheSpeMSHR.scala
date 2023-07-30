d1e2d0d88b1473e4ebe8cf6a1295fd2d29fa35c4
au_laptop_wsl <auchufeng@qq.com>
2023-07-30 14:12:57 +0800

    other SpeMSHR updates

--- ventus/src/L1Cache/DCache/DCacheSpeMSHR.scala
+++ ventus/src/L1Cache/DCache/DCacheSpeMSHR.scala
@@ -64,7 +64,7 @@ class SpeMSHR(val WIdBits: Int, val NMshrEntry:Int) extends Module{
     val probe = Flipped(ValidIO(new SpeMSHRprobe))
     val probeOut_st1 = Output(new SpeMSHRprobeOut(NMshrEntry))
     val missReq = Flipped(Decoupled(new SpeMSHRmissReq(WIdBits)))
-    val missRspIn = Flipped(Decoupled(new SpeMSHRmissRspIn(NMshrEntry)))
+    val missRspIn = Flipped(ValidIO(new SpeMSHRmissRspIn(NMshrEntry)))
     val missRspOut = ValidIO(new SpeMSHRmissRspOut(WIdBits))
     //For InOrFlu
     val empty = Output(Bool())
@@ -79,14 +79,13 @@ class SpeMSHR(val WIdBits: Int, val NMshrEntry:Int) extends Module{
   entryStatus.io.valid_list := Reverse(Cat(entry_valid))
 
   //  ******     missRsp status      ******
-  val subentryStatusForRsp = Module(new getEntryStatusRsp(NMshrEntry))
+  val entryStatusForRsp = Module(new getEntryStatusRsp(NMshrEntry))
 
   // ******     enum vec_mshr_status     ******
-  val mshrStatus_st1_r = RegInit(0.U(3.W))
-  val mshrStatus_st1_w = Wire(UInt(1.W))
+  val mshrStatus_st1_r = RegInit(0.U(1.W))
   /*AVAIL         000
   * FULL          001*/
-  // ******      mshr::probe_vec    ******
+  // ******      mshr::probe_spe    ******
   val entryFull = entryStatus.io.full
   val entryAlmFull = entryStatus.io.alm_full
   when(io.missReq.fire){
@@ -104,74 +103,36 @@ class SpeMSHR(val WIdBits: Int, val NMshrEntry:Int) extends Module{
       mshrStatus_st1_r := 0.U //AVAIL
     }
   }
-  when(secondaryMiss && (mshrStatus_st1_r === 0.U || mshrStatus_st1_r === 1.U)){
-    when(subEntryFull) {
-      mshrStatus_st1_w := 3.U //SECONDARY_FULL
-    }.otherwise {
-      mshrStatus_st1_w := 2.U //SECONDARY_AVAIL
-    }
-  }.otherwise{
-    mshrStatus_st1_w := mshrStatus_st1_r
-  }
-  io.probeOut_st1.probeStatus := mshrStatus_st1_w
 
-  //  ******     mshr::allocate_vec_sub/allocate_vec_main     ******
-  /*0:PRIMARY_AVAIL 1:PRIMARY_FULL 2:SECONDARY_AVAIL 3:SECONDARY_FULL*/
-  io.missReq.ready := !(mshrStatus_st1_w === 1.U || mshrStatus_st1_w === 3.U)// || io.missRspIn.valid)
+  io.probeOut_st1.probeStatus := mshrStatus_st1_r
+
+  //  ******     mshr::allocate_special     ******
+  /*0:AVAIL 1:FULL*/
+  io.missReq.ready := mshrStatus_st1_r === 0.U
   assert(!io.missReq.fire || (io.missReq.fire && !io.missRspIn.fire),"MSHR cant have Req & Rsp valid in same cycle, later the prior")
-  val real_SRAMAddrUp = Mux(secondaryMiss,OHToUInt(entryMatchProbe_st1),entryStatus.io.next)
-  val real_SRAMAddrDown = Mux(secondaryMiss,entryStatus.io.next,0.U)
-  when (io.missReq.fire){
-    targetInfo_Accesss(real_SRAMAddrUp)(real_SRAMAddrDown) := io.missReq.bits.targetInfo
-  }
 
-  when(io.missReq.fire && mshrStatus_st1_w === 0.U) {//PRIMARY_AVAIL
-    blockAddr_Access(entryStatus.io.next) := io.missReq.bits.blockAddr
+  when(io.missReq.fire){
     instrId_Access(entryStatus.io.next) := io.missReq.bits.instrId
   }
 
-  io.probeOut_st1.a_source := real_SRAMAddrUp
+  io.probeOut_st1.a_source := entryStatus.io.next
 
-  //  ******      mshr::vec_arrange_core_rsp    ******
-  subentryStatusForRsp.io.valid_list := Reverse(Cat(subentry_valid(entryMatchMissRsp)))
+  //  ******      mshr::special_arrange_core_rsp    ******
+  entryStatusForRsp.io.valid_list := Reverse(Cat(entry_valid))
   // priority: missRspIn > missReq
   //assert(!io.missRspIn.fire || (io.missRspIn.fire && subentryStatus.io.used >= 1.U))
   //This version allow missRspIn fire when no subentry are left
   //如果后面发现missRspOut端口这一级不能取消，使用这段注释掉的代码
-  //io.missRspIn.ready := !(subentryStatusForRsp.io.used >= 2.U ||
-  //  (subentryStatusForRsp.io.used === 1.U && !io.missRspOut.ready))
-  io.missRspIn.ready := !((subentryStatusForRsp.io.used >= 2.U) ||
-    ((mshrStatus_st1_w === 4.U || mshrStatus_st1_w === 3.U) && subentryStatusForRsp.io.used === 1.U))
 
-  entryMatchMissRsp := io.missRspIn.bits.instrId
-  //entryMatchMissRsp := Reverse(Cat(instrId_Access.map(_ === io.missRspIn.bits.instrId))) & entry_valid
-  //assert(PopCount(entryMatchMissRsp) <= 1.U,"MSHR missRspIn, cant match multiple entries")
-  val subentry_next2cancel = Wire(UInt(log2Up(NMshrSubEntry).W))
-  subentry_next2cancel := subentryStatusForRsp.io.next2cancel
-
-  io.missRspOut.bits.targetInfo := RegNext(missRspTargetInfo_st0)
-  io.missRspOut.bits.blockAddr := RegNext(missRspBlockAddr_st0)
-  io.missRspOut.bits.instrId := io.missRspIn.bits.instrId
+  io.missRspOut.bits.instrId := RegNext(io.missRspIn.bits.d_source)
   io.missRspOut.valid := RegNext(io.missRspIn.valid)
-  //io.missRspOut := RegNext(io.missRspIn.valid) &&
-  //  subentryStatusForRsp.io.used >= 1.U//如果上述Access中改出SRAM，本信号需要延迟一个周期
 
-  //  ******     maintain subentries    ******
-  /*0:PRIMARY_AVAIL 1:PRIMARY_FULL 2:SECONDARY_AVAIL 3:SECONDARY_FULL*/
+  //  ******     maintain entries    ******
   for (iofEn <- 0 until NMshrEntry){
-    for (iofSubEn <- 0 until NMshrSubEntry){
-      when(iofEn.asUInt===entryStatus.io.next &&
-        iofSubEn.asUInt===0.U && io.missReq.fire && primaryMiss){
-        subentry_valid(iofEn)(iofSubEn) := true.B
-      }.elsewhen(iofEn.asUInt===entryMatchMissRsp){
-        when(iofSubEn.asUInt===subentry_next2cancel &&
-          io.missRspIn.valid){
-          subentry_valid(iofEn)(iofSubEn) := false.B
-        }.elsewhen(iofSubEn.asUInt===entryStatus.io.next &&
-          io.missReq.fire && secondaryMiss){
-          subentry_valid(iofEn)(iofSubEn) := true.B
-        }
-      }//order of when & elsewhen matters, as elsewhen cover some cases of when, but no op to them
+    when(iofEn.asUInt === entryStatus.io.next && io.missReq.fire) {
+      entry_valid(iofEn) := true.B
+    }.elsewhen(iofEn.asUInt === io.missRspIn.bits.d_source && io.missRspIn.valid) {
+      entry_valid(iofEn) := false.B
     }
   }
 }
